5b62d8b5c91d3436aed666fae61cba77386e032d
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-03-10 13:07:40 -0300

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
-
+console.log("asd");
 export default function Home() {
   return (
     <>
